1445e84c4d060a3ab4dc7d96b5b12abe1d85e052
TECGRAF\jpecho <jpecho@tecgraf.puc-rio.br>
2024-10-28 19:43:30 -0300

    primer commit

---
+++ javagit/src/main/java/com/mycompany/javagit/Javagit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javagit;
+
+/**
+ *
+ * @author jpecho
+ */
+public class Javagit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
